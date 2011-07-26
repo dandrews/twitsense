6b33ecdd26445f11f05ea123bab87ee7633e9566
daniel <daniel@daniel-ThinkPad-X61s.(none)>
2011-07-25 23:18:08 -0600

    random string serving

--- src/twitsense.py
+++ src/twitsense.py
@@ -1,3 +1,18 @@
-frame_content = "<div><a target='_parent' href='https://twitter.com/#!/the_dan_bot'>'Good times with the twitter API'</a>-the_dan_bot</div>"
+from random import choice
+
+users = ["the_dan_bot","dandrewsify"]
+
+tweets = {'the_dan_bot':['Working with the twitter API',
+                         'The Twitter API is easy',
+                         'Good times with the twitter API'],
+          'dandrewsify':['Not Working with the twitter API',
+                         'The Twitter API is hard',
+                         'Bad times with the twitter API']
+          }
+
+user = choice(users)
+tweet = choice(tweets[user])
+
+frame_content = "<div><a target='_parent' href='https://twitter.com/#!/" + user + "'>'" + tweet + "'</a> -" + user + "</div>"
 
 print frame_content
