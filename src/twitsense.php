a904c247e54dde5c235ff1653eff38fe09162833
daniel <daniel@daniel-ThinkPad-X61s.(none)>
2011-07-30 14:49:45 -0600

    made testing script more generic for adding and removing content and generating html

--- src/twitsense.php
+++ src/twitsense.php
@@ -1,19 +1,28 @@
 <?php
 
 $tweets = array(
-                "<div><a target='_parent' href='https://twitter.com/#!/the_dan_bot'>'Good times with the twitter API'</a> @the_dan_bot</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>College ghost-writer uses online channels to help students cheat (and get away with it). http://t.co/fobRoBA</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>Montana ranked 2nd on 'most fit' list, right behind Alaska. http://bit.ly/raiiLz (via @krtv)</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>It kills me that people host with GoDaddy. Are they that great or do they just not know better?</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>What No One Seems to Mention About Google+ http://bit.ly/jxUorL</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>Wow! Here's a great way to run Internet Explorer on your Mac without a virtual machine. http://t.co/sJiDsEK</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>Cool! A vending machine that sells bike parts/tubes/food + free public workstation. http://dld.bz/agnB6</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>Announcing GitHub for Mac http://t.co/XNux4NY</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>Can anyone recommend any light/healthy cooking blogs and recipe sites? Need some inspiration on a daily basis...</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>Testing out Google's Music beta service. In the cloud baby, in the cloud!</a> @manifestphil</div>",
-                "<div><a target='_parent' href='https://twitter.com/#!/manifestphil'>I really hope that Microsoft doesn't bunk up Skype. :-( It was just getting really good on Mac.</a> @manifestphil</div>"
-                     );
+                array( "Good times with the twitter API", "the_dan_bot" ),
+                array("College ghost-writer uses online channels to help students cheat (and get away with it). http://t.co/fobRoBA", "manifestphil"),
+                array("Montana ranked 2nd on 'most fit' list, right behind Alaska. http://bit.ly/raiiLz (via @krtv)","manifestphil"),
+                array("It kills me that people host with GoDaddy. Are they that great or do they just not know better?","manifestphil"),
+                array("What No One Seems to Mention About Google+ http://bit.ly/jxUorL", "manifestphil"),
+                array("Wow! Here's a great way to run Internet Explorer on your Mac without a virtual machine. http://t.co/sJiDsEK","manifestphil"),
+                array("Cool! A vending machine that sells bike parts/tubes/food + free public workstation. http://dld.bz/agnB6", "manifestphil"),
+                array("Announcing GitHub for Mac http://t.co/XNux4NY", "manifestphil"),
+                array("Can anyone recommend any light/healthy cooking blogs and recipe sites? Need some inspiration on a daily basis...", "manifestphil"),
+                array("Testing out Google's Music beta service. In the cloud baby, in the cloud!", "manifestphil"),
+                array("I really hope that Microsoft doesn't bunk up Skype. :-( It was just getting really good on Mac.", "manifestphil")
+                );
 
-echo $tweets[ rand( 0, count($tweets)-1 ) ];
+$pair = $tweets[ rand( 0, count($tweets)-1 ) ];
+
+$tweet = $pair[0];
+$user = $pair[1];
+
+$html = "<div><a target='_parent' href='https://twitter.com/#!/{$user}'>{$tweet}</a> @{$user}</div>";
+
+echo $html;
+
+// return $html;
 
 ?>
