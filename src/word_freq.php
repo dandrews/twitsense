74d8eea94a08b2cb2aefdbb89bf4a2972d3051d7
daniel <daniel@daniel-ThinkPad-X61s.(none)>
2011-08-03 00:42:56 -0600

    get word frequency from text strings

--- src/word_freq.php
+++ src/word_freq.php
@@ -1,13 +1,40 @@
 <?php
 
-$str = 'happy beautiful happy lines pear gin happy lines rock happy lines pear ';
+$str = "";
 
+// read from a source of words
+$file_handle = fopen("sample_blog_post.txt", "r");
+
+while (!feof($file_handle)) {
+  $str .=  strtolower(fgets($file_handle));
+}
+
+fclose($file_handle);
+
+
+// now do the counting
 $words = array_count_values(str_word_count($str, 1));
-
-print_r($words);
 
 arsort($words);
 
-print_r($words);
+$top_words = array();
+
+$stopwords_arr = array("to", "in", "each", "and", "a", "s", "t", "is", "if", "you", "your", "the", "this", "they", "that");
+
+
+// filter out infrequently use and unimportant words
+foreach ($words as $key => $val )
+{
+  if (($val >= 5) && (!in_array($key, $stopwords_arr)))
+  {
+    array_push( $top_words, $key );
+  }
+}
+
+// only take the top 5 words
+$top_words = array_slice($top_words, 0, 5);
+
+// at this point, the first word is the most common, the second is ...
+print_r($top_words);
 
 ?>
